a4bcaeb281c530e0342c6477e3507176e0fda1d4
adrianmaurer <adriano.maurer@cbc.ca>
2014-05-21 11:16:05 -0400

    new pre build target for setup

---
+++ PreBotSetup/PreBotSetup.h
@@ -0,0 +1,13 @@
+//
+//  PreBotSetup.h
+//  PreBotSetup
+//
+//  Created by Adrian Maurer on 5/21/14.
+//  Copyright (c) 2014 com.verticodelabs. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PreBotSetup : NSObject
+
+@end
---
+++ PreBotSetup/PreBotSetup.m
@@ -0,0 +1,13 @@
+//
+//  PreBotSetup.m
+//  PreBotSetup
+//
+//  Created by Adrian Maurer on 5/21/14.
+//  Copyright (c) 2014 com.verticodelabs. All rights reserved.
+//
+
+#import "PreBotSetup.h"
+
+@implementation PreBotSetup
+
+@end
